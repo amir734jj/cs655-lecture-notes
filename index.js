2eb8ec54ccb3c015dfbe67810a78c1b3544d8b57
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-04-06 17:12:45 -0500

    added videos

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const findFiles = require('file-regex')
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000
 
-let files = [];
+function flatten(arr) {
+    return arr.reduce(function(flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+}
+
+
+let payload = {files:[], videos:[]};
 findFiles(__dirname, /lab\d+\/\w+\.pdf/g, 1)
-    .then(file => {
-        files = file.map(props => {
+    .then(files => {
+        payload.files = files.map(props => {
             const path = props.dir.substring(props.dir.lastIndexOf('/') + 1);
             return {
                 name: path,
                 url: path + '/' + props.file
             }
-        })
-
-        files.sort((x, y) => x.name.localeCompare(y.name))
+        }).sort((x, y) => x.name.localeCompare(y.name))
     })
+
+findFiles(__dirname, /lab\d+\/video\.json/g, 1)
+    .then(videos => {
+        payload.videos = flatten(videos.map(props => {
+            return JSON.parse(fs.readFileSync(path.join(props.dir, props.file), {
+                encoding: 'utf8'
+            }))
+        })).reverse();
+    })
+
 
 app.use(express.static('.'))
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        'files': files
-    });
+    res.render('index', payload);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
