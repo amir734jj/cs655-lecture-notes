e16fa6697e40bf43a2e10fcf4f6599a6c22e3b53
Amir <hesamian@uwm.edu>
2021-10-24 22:11:59 -0500

    Trying to fix the build

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,16 @@
 var glob = require("glob");
-var cp = require("child_process");
+const execa = require("execa");
 
 glob("lab*/*.marp.md", {}, function (err, files) {
   files.forEach((file) => {
     var destination = file.replace(".md", "");
-    var cmd = `CHROME_DRIVER=$GOOGLE_CHROME_BIN marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
-    var result = cp.execSync(cmd);
-    console.log(result.toString());
+    var cmd = `marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
+    var result = execa.commandSync(cmd, {
+      ...process.env,
+      CHROME_DRIVER: "$GOOGLE_CHROME_BIN",
+    });
+    console.log(
+      `[${result.failed ? "failed" : "success"}] ${result.command}`
+    );
   });
 });
