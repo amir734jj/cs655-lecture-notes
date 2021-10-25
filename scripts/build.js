1d8902131f3f5faa1655919ee9c7f6e0db2c6e88
Amir <hesamian@uwm.edu>
2021-10-24 22:46:36 -0500

    testing

--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,7 @@ glob("lab*/*.marp.md", {}, function (err, files) {
     var cmd = `marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
     var result = execa.commandSync(cmd, {
       ...process.env,
-      CHROME_PATH: "/app/.apt/usr/bin/google-chrome",
+      CHROME_PATH: "$GOOGLE_CHROME_BIN",
     });
     console.log(
       `[${result.failed ? "failed" : "success"}] ${result.command}`
