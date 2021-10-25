2a7fc827e2875145da138cf2f15a516087469a63
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2021-10-24 22:00:01 -0500

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@ var cp = require("child_process");
 glob("lab*/*.marp.md", {}, function (err, files) {
   files.forEach((file) => {
     var destination = file.replace(".md", "");
-    var cmd = `marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
+    var cmd = `CHROME_DRIVER=$GOOGLE_CHROME_BIN marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
     var result = cp.execSync(cmd);
     console.log(result.toString());
   });
