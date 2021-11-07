4100471ebe34972ba39a55904496e7b008745987
Amir <hesamian@uwm.edu>
2021-11-07 00:08:09 -0500

    testing

--- scripts/build.js
+++ scripts/build.js
@@ -2,22 +2,13 @@ var glob = require("glob");
 var execa = require("execa");
 var fs = require("fs");
 
-console.log(
-  `${process.env.GOOGLE_CHROME_SHIM}: exists? ${fs.existsSync(
-    process.env.GOOGLE_CHROME_SHIM
-  )}`
-);
-
-console.log(JSON.stringify(process.env));
-
 glob("lab*/*.marp.md", {}, function (err, files) {
   files.forEach((file) => {
     var destination = file.replace(".md", "");
     var cmd = `marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
     var result = execa.commandSync(cmd, {
       ...process.env,
-      CHROME_PATH:
-        process.env.GOOGLE_CHROME_BIN || "/app/.apt/opt/google/chrome/chrome",
+      CHROME_PATH: process.env.GOOGLE_CHROME_BIN,
     });
     console.log(`[${result.failed ? "failed" : "success"}] ${result.command}`);
   });
