fbb62ee80eb2eb9bfa780ae9a64740eeac921a1b
Amir <hesamian@uwm.edu>
2021-11-06 23:46:08 -0500

    losing hope

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,8 @@ var execa = require("execa");
 var fs = require("fs");
 
 console.log(
-  `${process.env.GOOGLE_CHROME_BIN}: exists? ${fs.existsSync(
-    process.env.GOOGLE_CHROME_BIN
+  `${process.env.GOOGLE_CHROME_SHIM}: exists? ${fs.existsSync(
+    process.env.GOOGLE_CHROME_SHIM
   )}`
 );
 
@@ -14,7 +14,8 @@ glob("lab*/*.marp.md", {}, function (err, files) {
     var cmd = `marp ${file} --allow-local-files --pdf -o ${destination}.pdf`;
     var result = execa.commandSync(cmd, {
       ...process.env,
-      CHROME_PATH: process.env.GOOGLE_CHROME_BIN,
+      CHROME_PATH:
+        process.env.GOOGLE_CHROME_BIN || "/app/.apt/opt/google/chrome/chrome",
     });
     console.log(`[${result.failed ? "failed" : "success"}] ${result.command}`);
   });
